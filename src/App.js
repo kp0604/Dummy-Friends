53b30f2ad5c46e7397ff791d7d2046b91cedbb3e
kartikeya pandey <pandeykartikeya0604@gmail.com>
2021-04-01 01:55:37 +0530

    -bug fixes

--- src/App.js
+++ src/App.js
@@ -47,8 +47,8 @@ function App() {
         {/* <UserCard userObj={userObj} friendObj={friendObj} setFriendObj={(fD) => setFriendObj(fD)} fetchData={() => fetchData()} /> */}
         <Switch>
           <Route path="/" component={() => <UserCard userObj={userObj} friendObj={friendObj} setFriendObj={(fD) => setFriendObj(fD)} fetchData={() => fetchData()} />} />
-          <Route path="/home" component={() => <UserCard userObj={userObj} friendObj={friendObj} setFriendObj={(fD) => setFriendObj(fD)} fetchData={() => fetchData()} />} />
-          <Route path="/friends" component={() => <Friends friendObj={friendObj} />} />
+          <Route exact path="/home" component={() => <UserCard userObj={userObj} friendObj={friendObj} setFriendObj={(fD) => setFriendObj(fD)} fetchData={() => fetchData()} />} />
+          <Route excat path="/friends" component={() => <Friends friendObj={friendObj} />} />
           <Redirect path="/" />
         </Switch>
         <Footer />
